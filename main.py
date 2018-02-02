9a53dc272ee63a4931880fafc626af72ee5c85f2
Sebastian Borgeaud <spb61@cl.cam.ac.uk>
2018-02-02 10:21:09 +0000

    Add data augmentation

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from data import load_data
 from model import get_model
 from cnn_model import get_cnn_model
 
+from keras.preprocessing.image import ImageDataGenerator
+
 batch_size = 128
-nb_epoch = 10
+nb_epoch = 30
 
 # Load data
 (X_train, y_train, X_test, y_test) = load_data()
@@ -16,11 +18,15 @@ nb_epoch = 10
 # Load and compile model
 model = get_cnn_model()
 
-model.compile(loss='categorical_crossentropy', optimizer=RMSprop(),
+model.compile(loss='categorical_crossentropy', optimizer=Adam(),
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epoch,
-          verbose=1, validation_data=(X_test, y_test))
+#model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epoch, verbose=1, validation_data=(X_test, y_test))
+
+datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1)
+
+datagen.fit(X_train)
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size), epochs=nb_epoch, verbose=1, validation_data=(X_test, y_test))
 
 score = model.evaluate(X_test, y_test, verbose=1)
 
